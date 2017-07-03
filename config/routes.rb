0fd97fcfedbe01ca358a9434993a46e32d742e0e
Irene Li <ireneaili@berkeley.edu>
2017-07-03 05:24:58 +0000

    Test: Added new Welcome route to test and make the page not empty when running

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  get 'welcome/index'
+  
+  root 'welcome#index'
+
   mount JasmineRails::Engine => '/specs' if defined?(JasmineRails)
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
