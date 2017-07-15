5c0a0d9f8a937634bb608d565fa41011b1eb85ee
Tesfahun Berhanu <tberhanu@berkeley.edu>
2017-07-15 23:59:37 +0000

    unidentified bug still there about the button link

--- features/login_as_CL_User.feature
+++ features/login_as_CL_User.feature
@@ -3,21 +3,28 @@ Feature: able to login as cl_user
   As a community life user I should be able
   to put my email and password and login
   
+  
+Background:
+  Given that I have a user with a valid credentials placed in the correct forms:
+  When I press "Sign up"
+  Then I should see "Sign out"
+    
+  
 # Login with invalid username
 Scenario: unable to login with invalid username
   Given invalid cl_user_email and a password
   When I press "Sign in"
-  Then I should see "Bad email or password."
+  Then I should see "Incorrect email and/or password."
   
  # Login with valid username and password
 Scenario: able to login with valid username and correct password
-  Given correct cl_user_email and password
-#  When I press "login"
-#  Then I should see "#cl_user"
+  Given correct cl_user_email and password 
+  When I press "Sign in"
+  Then I should see "Sign out"
   
 # Login with valid username and incorrect password  
 Scenario: unable to login with valid username and incorrect password
   Given correct cl_user_email and password
-#  When I press "login"
-#  Then I should see "Invalid email address and/or password"
+  # When I press "login"
+  # Then I should see "Sign out"
 
--- features/step_definitions/login_as_CL_steps.rb
+++ features/step_definitions/login_as_CL_steps.rb
@@ -10,6 +10,22 @@ module WithinHelpers
 end
 World(WithinHelpers)
 
+
+
+  
+  #now we have a user within this
+Given(/^that I have a user with a valid credentials placed in the correct forms:$/) do
+    # creating a user 
+  visit "/sign_in"
+  click_link("submit_sign_in")
+  #find('submit_sign_in').click_link("Sign in")
+  fill_in('Email', :with => 'us@domain.com')
+  fill_in('Password', :with => 'us')
+  click_button 'submit_sign_in'
+  # find('submit_sign_in').click_link("Sign in")
+end
+
+
 Given(/^invalid cl_user_email and a password$/) do
   # pending # Write code here that turns the phrase above into concrete actions
   visit "/sign_in"
@@ -17,11 +33,18 @@ Given(/^invalid cl_user_email and a password$/) do
   fill_in('Password', :with => 'us')
   click_button 'button-cap'
   
-  # page.should have_content("Invalid email address and/or password")
-  page.should have_content("Sign in")
+  page.should have_content("Incorrect email and/or password.")
+  #page.should have_content("Sign in")
   
 end
 
 Given(/^correct cl_user_email and password$/) do
-  # pending # Write code here that turns the phrase above into concrete actions
+  visit "/sign_in"
+  fill_in('Email', :with => 'uk@domain.com')
+  fill_in('Password', :with => 'uk')
+  click_button 'button-cap'
+  # visit "/"
+  page.should have_content("Signed in as: uk@domain.com")
+  # Signed in
+
 end
