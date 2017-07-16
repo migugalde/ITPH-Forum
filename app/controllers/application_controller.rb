d49df0a2f81b81eed3d5d83cb036dc1530b2f605
Irene Li <ireneaili@berkeley.edu>
2017-07-16 19:53:17 +0000

    EDIT: added in keys to the signup form through configure_permitted_parameters in application_controller.rb so that when you sign up, the first and last name appear in the user database

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   after_filter :store_action
+  before_action :configure_permitted_parameters, if: :devise_controller?
   
   def store_action
     return unless request.get? 
@@ -35,6 +36,13 @@ class ApplicationController < ActionController::Base
     else
       redirect_to community_index_path, :notice => 'You do not have the rights to access this page. Please contact the admin.'
     end
+  end
+  
+ 
+
+  protected
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [[:first_name], [:last_name]])
   end
   
 end
