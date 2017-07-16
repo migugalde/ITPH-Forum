9b436f0401536955df891ba1952c610f89323170
Edwan <edwanhernandez@gmail.com>
2017-07-16 01:52:45 +0000

    FIX: changed Background into a scenari
    It was causing errors for other scenarios
    that had the assumption that they were unsucesful at logging in.

--- features/step_definitions/login_as_CL_steps.rb
+++ features/step_definitions/login_as_CL_steps.rb
@@ -28,7 +28,7 @@ Given(/^invalid cl_user_email and a password$/) do
   # pending # Write code here that turns the phrase above into concrete actions
   visit "/sign_in"
   fill_in('Email', :with => 'us@domain.com')
-  fill_in('Password', :with => 'us')
+  fill_in('Password', :with => 'usa')
   click_button 'button-cap'
   
   page.should have_content("Incorrect email and/or password.")
