194c6747175711c25907b5be053d8d819943e432
Irene Li <ireneaili@berkeley.edu>
2017-07-16 21:35:03 +0000

    ADD: added flash messages for: user trying to access admin pages, user successfully signing out, user inputting email and password incorrectly, email sent if approved, email sent if unapproved, if account has not been approved yet

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -23,7 +23,7 @@ class ApplicationController < ActionController::Base
     if user_signed_in?
       super
     else
-      redirect_to root_path, :notice => 'You must be logged in to access this page'
+      redirect_to root_path, :alert => 'You must be logged in to access this page'
       ## if you want render 404 page
       ## render :file => File.join(Rails.root, 'public/404'), :formats => [:html], :status => 404, :layout => false
     end
@@ -34,12 +34,10 @@ class ApplicationController < ActionController::Base
     if user_signed_in? && current_user.admin
       authenticate_user!
     else
-      redirect_to community_index_path, :notice => 'You do not have the rights to access this page. Please contact the admin.'
+      redirect_to community_index_path, :alert => 'You do not have the rights to access this page. Please contact the admin.'
     end
   end
-  
  
-
   protected
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [[:first_name], [:last_name]])
