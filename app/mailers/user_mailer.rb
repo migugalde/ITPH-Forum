21b2e57c0e7856c3d4410a13ddf20e7d8e855840
Irene Li <ireneaili@berkeley.edu>
2017-07-16 10:42:35 +0000

    ADD: set up some mailer stuff so that when the admin grants access or removes access, an email is sent to the user

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -3,7 +3,12 @@ class UserMailer < ApplicationMailer
  
     def welcome_email(user)
         @user = user
-        @url  = show_path
+        @url  = root_path
         mail(to: @user.email, subject: 'Welcome to the ITPH Community!')
+    end
+    
+    def unapproved_email(user)
+        @user = user
+        mail(to: @user.email, subject: 'An ITPH admin has revoked your access.')
     end
 end
