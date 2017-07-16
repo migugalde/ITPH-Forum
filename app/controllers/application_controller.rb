f2adb353414b4bc066c7f0369f2c8801bd60230e
Irene Li <ireneaili@berkeley.edu>
2017-07-16 22:39:14 +0000

    EDIT: fixed the welcome view to not include sign in. changed the argument for afer_sign_in_path_for(*) for codeclimate

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   
-  def after_sign_in_path_for(resource_or_scope)
+  def after_sign_in_path_for(*)
     community_index_path
   end
   
