1119c72d1d4b73b4b57a89083b73cbb801048c39
Miguel Ugalde <migugalde@berkeley.edu>
2017-07-20 02:15:00 +0000

    Fixed: cucumber step definition ambiguity

--- features/step_definitions/forum_listings_steps.rb
+++ features/step_definitions/forum_listings_steps.rb
@@ -19,13 +19,13 @@ Then(/^I should see "([^"]*)" before "([^"]*)"$/) do |arg1, arg2|
   pending # Write code here that turns the phrase above into concrete actions
 end
 
-When(/^I follow "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# When(/^I follow "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
 
-Then(/^I should not see "([^"]*)"$/) do |arg1|
-  pending # Write code here that turns the phrase above into concrete actions
-end
+# Then(/^I should not see "([^"]*)"$/) do |arg1|
+#   pending # Write code here that turns the phrase above into concrete actions
+# end
 
 Then(/^I should be on the forum post page$/) do
   pending # Write code here that turns the phrase above into concrete actions
