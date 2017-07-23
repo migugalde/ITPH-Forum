3fa4b29584a26a70b3d0006609d3e4f47785f241
Irene Li <ireneaili@berkeley.edu>
2017-07-23 19:46:16 +0000

    FIX: fixed a problem with signing up

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -2,6 +2,9 @@ class CommentsController < ApplicationController
     def create
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(params[:comment].permit(:comment))
+        @comment.user_id = current_user.id if current_user
+        @user = User.find(@comment.user_id)
+        @comment.save
          if @comment.save
             redirect_to post_path(@post)
         else
