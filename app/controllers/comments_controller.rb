faf4189042a9928d86cd7e4aef2d4407db3d1211
Omkar Raje <oraje@berkeley.edu>
2017-07-23 21:31:16 +0000

    email for reply

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,14 +1,17 @@
 class CommentsController < ApplicationController
+    
+    
     def create
-        @post = Post.find(params[:post_id])
-        @comment = @post.comments.create(params[:comment].permit(:comment))
-        @comment.user_id = current_user.id if current_user
-        @user = User.find(@comment.user_id)
-        @comment.save
-         if @comment.save
-            redirect_to post_path(@post)
-        else
-            render 'new'
-        end
-    end
+		@post = Post.find(params[:post_id])
+		@comment = @post.comments.create(params[:comment].permit(:comment))
+		@comment.user_id = current_user.id if current_user
+		@comment.save
+
+		if @comment.save
+			redirect_to post_path(@post)
+		else
+			render 'new'
+		end
+	end
+
 end
