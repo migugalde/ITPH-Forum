4d443996e10c9bf727dbcd82a8da34b9ef750411
Irene Li <ireneaili@berkeley.edu>
2017-07-24 04:25:23 +0000

    ADD: added admin posts in the community view

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -1,5 +1,7 @@
 class CommunityController < ApplicationController
     before_filter :authenticate_user!
     def index
+        @user = User.where(admin: true)
+        @posts = Post.where(user_id: @user)
     end
 end
