2b978c1c685825b599e765959d63ec4a5801ed95
Irene Li <ireneaili@berkeley.edu>
2017-07-24 01:27:12 +0000

    added sort by on the post page

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,8 @@ class PostsController < ApplicationController
     before_action :authenticate_user!, except: [:index, :show]
     
     def index
-        @posts = Post.all.order("created_at DESC") 
+        @posts = Post.all.order(params[:sort])
+        
     end
     
     def show 
