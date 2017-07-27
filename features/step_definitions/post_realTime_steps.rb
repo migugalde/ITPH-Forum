0ec15ba1a48683caadcbffb855c5e82cef784807
edwann13 <edwanhernandez@gmail.com>
2017-07-26 23:17:48 -0700

    add: cucumber tests for real-time updates for posts
    Added cucmber tests for real-time updates for all users
    that are currently "online" aka subscibed.

---
+++ features/step_definitions/post_realTime_steps.rb
@@ -0,0 +1,15 @@
+Given(/^that two different users are logged in$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^that user_one is ready to submit a new post$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^user_one press "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^user_two should see "([^"]*)"$/) do |arg1|
+  pending # Write code here that turns the phrase above into concrete actions
+end
