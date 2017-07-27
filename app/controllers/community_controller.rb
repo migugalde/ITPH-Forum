6e96f39ed66ed69c89fc5349bd43389f06e8b16e
Irene Li <ireneaili@berkeley.edu>
2017-07-27 10:57:24 +0000

    'quick: Indented to fix a styling issue with buttons':

--- app/controllers/community_controller.rb
+++ app/controllers/community_controller.rb
@@ -2,6 +2,6 @@ class CommunityController < ApplicationController
     before_filter :authenticate_user!
     def index
         @user = User.where(admin: true)
-        @posts = Post.where(user_id: @user)
+        @posts = Post.where(user_id: @user).last(3)
     end
 end
