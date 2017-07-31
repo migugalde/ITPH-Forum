320151b9e990b0ba9c36ddc207e6c9b8e652b17d
edwann13 <edwanhernandez@gmail.com>
2017-07-30 21:17:25 -0700

    fix: forgot one merge conflict.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -46,20 +46,13 @@ Rails.application.configure do
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
-<<<<<<< HEAD
 
-
-=======
->>>>>>> bed6e8920a30af65dadb583a11310b5d8a398b8c
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
-<<<<<<< HEAD
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-=======
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
->>>>>>> bed6e8920a30af65dadb583a11310b5d8a398b8c
 end
