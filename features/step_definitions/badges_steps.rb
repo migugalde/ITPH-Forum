d97021718b730563ac8bcca2c5a16cfac3932e5e
Edwan <edwanhernandez@gmail.com>
2017-08-02 23:34:15 -0700

    add: creating cucumber tests for badges (#37)
    created tests for iter 4 for .features and steps.rb for the badges
    feature

---
+++ features/step_definitions/badges_steps.rb
@@ -0,0 +1,38 @@
+require 'uri'
+require 'cgi'
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+Given(/^that the user finishes their first goal$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the user will have a badge appear on their profile$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^that atleast one user has won a badge$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+When(/^they visit badges page$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^they will see a new row$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^that a user lies about their accomplishment$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Then(/^the admin will revoke the badge$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+Given(/^that I have a user with a valid credentials placed in the correct forms:$/) do
+  pending # Write code here that turns the phrase above into concrete actions
+end
+
+
