6986f14ba1e234fa7b692bcb90ea10d131ce63f8
Omkar Raje <oraje@berkeley.edu>
2017-08-04 03:10:25 +0000

    maint:  fixed image resolution

--- app/uploaders/avatar_uploader.rb
+++ app/uploaders/avatar_uploader.rb
@@ -5,6 +5,7 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
   include CarrierWave::MiniMagick
+  
 
   # Choose what kind of storage to use for this uploader:
   storage :file
@@ -33,14 +34,21 @@ class AvatarUploader < CarrierWave::Uploader::Base
 
   # Create different versions of your uploaded files:
   version :thumb do
-    process :resize_to_fit => [50, 50]
+    process :resize_to_fit => [150, 150]
   end
+  
+  
+
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   def extension_white_list
     %w(jpg jpeg gif png)
   end
+  
+  private
+
+  
 
   # Override the filename of the uploaded files:
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
