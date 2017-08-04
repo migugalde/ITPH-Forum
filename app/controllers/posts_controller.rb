8c245d57cf8f8e87c77069e19774cd8601d36755
edwann13 <edwanhernandez@gmail.com>
2017-08-04 12:50:16 -0700

    fix: removed old code that was commented out

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -40,7 +40,6 @@ class PostsController < ApplicationController
     
     def new
         @post = Post.new
-        # @post = current_user.posts.build
     end
      
     def edit
@@ -65,11 +64,7 @@ class PostsController < ApplicationController
 
     def create
         @post = current_user.posts.build(post_params)
-        #@post = Post.new(post_params)
         if @post.save
-            # rubyJSON = {:title => @post.title, :content => @post.content}.to_json
-            # rubyJSON = {:title => "title", :content => "body"}.to_json
-            # PostsBroadcastJob.perform_now(rubyJSON)
             PostsBroadcastJob.perform_now(@post)
             redirect_to @post
         else
