f7aaa5933af641bebbf39b7e4ae5aa3c989a371e
Irene Li <ireneaili@berkeley.edu>
2017-08-05 09:28:58 +0000

    feat:  added a default avatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
     #attr_accessible :first_name, :last_name, :username, :password, :email, :privilege
     devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: ":style/missing.png"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
     
     has_many :posts
