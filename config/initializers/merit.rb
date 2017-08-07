56755b46ee8574ef7821f7fd8155cc5dd2ea2b2d
edwann13 <edwanhernandez@gmail.com>
2017-08-06 22:56:57 -0700

    impr: added updated images paths

--- config/initializers/merit.rb
+++ config/initializers/merit.rb
@@ -23,66 +23,76 @@ end
 Merit::Badge.create!(
   id: 1,
   name: 'Novice Poster',
-  # name: 'badge-icon.png',
-  description: "You have written a post!"
-  # custom_fields: { url: "badge-icon.png" }
+  level: "1",
+  description: "badge1.png"
 )
 
 Merit::Badge.create!(
   id: 2,
   name: 'Intermediate Poster',
-  # name: 'Intermediate Poster: You have created 2 or more posts!',
-  # name: 'badge-icon.png',
-  level: "90",
-  description: "badge-icon.png",
-  custom_fields: { url: "badge-icon.png" }
+  level: "82",
+  description: "badge1.png"
 
 )
 
 Merit::Badge.create!(
   id: 3,
   name: 'Experienced Poster',
-  description: "You have created 5 or more posts!"
+  level: "164",
+  description: "badge1.png"
 )
 
 Merit::Badge.create!(
   id: 4,
   name: 'Pro Poster',
-  description: "You have created 10 or more posts!"
+  level: "246",
+  description: "badge1.png"
 )
 
 Merit::Badge.create!(
   id: 5,
   name: 'Ultimate Poster',
-  description: "You have created 25 or more posts!"
+  level: "328",
+  description: "badge1.png"
 )
+
+
+
+
+
+
 
 Merit::Badge.create!(
   id: 6,
   name: "Novice Commenter",
-  description: "You have commented more than Once!"
+  level: "1",
+  description: "badge2.png"
   )
 
 Merit::Badge.create!(
   id: 7,
   name: "Intermediate Commenter",
-  description: "You have commented more than 5 times"
+  level: "82",
+  description: "badge2.png"
   )
 
 Merit::Badge.create!(
   id: 8,
   name: "Experienced Commenter",
-  description: "You have commented more than 15 times"
+  level: "164",
+  description: "badge2.png"
   )
 
 Merit::Badge.create!(
   id: 9,
   name: "Pro Commenter",
-  description: "You have commented more than 25 times"
+  level: "246",
+  description: "badge2.png"
   )
 
 Merit::Badge.create!(
   id: 10,
   name: "Ultimate Commenter",
-  description: "You have commented more than 50 times"
+  level: "328",
+  description: "badge2.png"
   )
