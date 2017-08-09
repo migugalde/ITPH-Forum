d2530dbed9e46b2237cb433149a209870bb39085
Irene Li <ireneaili@berkeley.edu>
2017-08-09 23:35:12 +0000

    feat:  adding aws for paperclip

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -117,4 +117,14 @@ Rails.application.configure do
 		api_key: 'key-e796c3d50e04aaaa1c1a96d5bba9a427',
 		domain: 'sandbox7d55082f254c4626bd71d56bbe0dd9a7.mailgun.org'
   }
+  
+  config.paperclip_defaults = {
+    storage: :s3,
+    s3_credentials: {
+      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
+      secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+      s3_region: ENV.fetch('AWS_REGION'),
+    }
+}
 end
