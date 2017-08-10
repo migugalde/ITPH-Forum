8a9cb017bbe5b5b6355c63d890ee160006158225
Miguel Ugalde <migugalde@berkeley.edu>
2017-08-10 23:19:35 +0000

    fix: taking away broken mailer

--- app/controllers/my_registrations_controller.rb
+++ app/controllers/my_registrations_controller.rb
@@ -1,11 +1,11 @@
 class MyRegistrationsController < Devise::RegistrationsController
 
-    def create
-        super
-        if @user.persisted?
-          @admin = User.where(admin: true).first
-          UserMailer.user_created_email(@admin).deliver_now
-        end
-    end
+    # def create
+    #     super
+    #     if @user.persisted?
+    #       @admin = User.where(admin: true).first
+    #       UserMailer.user_created_email(@admin).deliver_now
+    #     end
+    # end
     
 end
